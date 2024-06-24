b1eb079c6bee035ba1ded9561f13b18dd06f5bae
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2024-06-24 18:51:36 +0530

    Modifications to alignments

--- app/reportgen/common.tsx
+++ app/reportgen/common.tsx
@@ -1,12 +1,19 @@
-export default function Common() {
+export function Common() {
     return <div className="flex flex-col gap-8 items-center">
         <p className="text-white font-bold">
          Just A Few Steps Away From Getting Your Report Ready !
         </p>
-        <div className="flex items-center">
-            <button className="text-white font-bold border-2 px-4 text-md py-2 rounded-[100%]">1</button>
-            <div className="w-28 border border-gray-500"></div>
-            <button className="text-gray-500 font-bold border-2 border-gray-500 px-4 text-md py-2 rounded-[100%]">2</button>
-        </div>
     </div>
+}
+
+export function Progress() {
+   return <>
+            <button className="text-white font-bold border-2 px-4 text-md py-2 rounded-[100%]">
+              1
+            </button>
+            <div className="w-28 border border-gray-500"></div>
+            <button className="text-gray-500 font-bold border-2 border-gray-500 px-4 text-md py-2 rounded-[100%]">
+              2
+            </button>
+   </> 
 }
--- app/reportgen/page.tsx
+++ app/reportgen/page.tsx
@@ -1,20 +1,28 @@
 import Logo2 from "../Components/Images/Logo2";
 import Navbar from "../Components/Navbar";
-import Common from "./common";
+import { Common, Progress } from "./common";
 import Step1 from "./step1";
 
 export default function Page() {
-    return <div className="h-screen pt-6 bg-[#01162B] grid grid-rows-[230px_auto] grid-cols-2">
-        <div className="grid grid-cols-4 grid-rows-[100px_50px_80px] row-start-1 row-end-3 col-start-1 col-end-3">
-            <div className="pl-10 row-start-1 row-end-3 col-start-1 col-end-2">
-               <Logo2/>
-            </div>
-           <div className="w-full justify-self-end col-start-2 col-end-5 max-w-[500px] ">
-               <Navbar/>
-           </div>
-            <div className="self-start place-content-start row-start-2 row-end-3 col-start-2 col-end-4">
-                <Common/>
-            </div>
+  return (
+    <div className="h-screen pt-6 bg-[#01162B] grid grid-rows-[230px_0px] grid-cols-2">
+      <div className="grid grid-cols-4 grid-rows-[100px_50px_80px] row-start-1 row-end-3 col-start-1 col-end-3">
+        <div className="pl-10 row-start-1 row-end-3 col-start-1 col-end-2">
+          <Logo2 />
         </div>
+        <div className="w-full justify-self-end col-start-2 col-end-5 max-w-[500px] ">
+          <Navbar />
+        </div>
+        <div className="self-end place-content-end row-start-2 row-end-3 col-start-2 col-end-4">
+          <Common />
+        </div>
+          <div className="flex items-center justify-center row-start-3 row-end-4 col-start-2 col-end-4">
+          <Progress/>
+        </div>
+      </div>
+      <div>
+        <Step1 />
+      </div>
     </div>
+  );
 }
