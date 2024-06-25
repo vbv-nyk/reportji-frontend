36b4128d9e3599aeb7c7cbbb2c290292c06acda6
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2024-06-25 18:28:33 +0530

    Accordion

---
+++ app/reportgen/Accordion/accordion.tsx
@@ -0,0 +1,9 @@
+import ButtonYellow1 from "@/app/Components/Buttons/ButtonYellow1";
+import ButtonYellow2 from "@/app/Components/Buttons/ButtonYellow2";
+import getElementName from "@/app/types/elements";
+import { PdfElement } from "@/app/types/types";
+
+export default function Accordion({ elements }: { elements: PdfElement[] }) {
+    return <>
+    </>
+}
