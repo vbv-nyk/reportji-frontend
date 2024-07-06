5e8a1bf8e8ee630c7cc5d585b6e3a649e1022769
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2024-07-06 18:04:18 +0530

    Display username on sign in

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,16 +1,58 @@
-"use client"
+"use client";
+import { useEffect, useState } from "react";
 import { BACKEND_PORT, BACKEND_URL } from "../constants";
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 export default function Navbar() {
   async function login() {
     window.open(`${BACKEND_URL}/auth/google`, "_self");
   }
+  
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+  const client = new ApolloClient({
+    uri: `${BACKEND_URL}/graphql`,
+    cache: new InMemoryCache(),
+    credentials: "include"
+  });
+    async function getUserData() {
+      try {
+        const data = await client.query({
+          query: gql`
+            query UserDetails {
+              UserDetails {
+                displayName
+                id
+              }
+            }
+          `,
+        });
+         setUsername(data.data.UserDetails.displayName);
+        return data;
+      } catch (e) {
+        console.log("Error" + e);
+      }
+    }
+    getUserData();
+  }, []);
   return (
     <div className="flex flex-wrap text-sm justify-center gap-2 px-2 items-center md:px-0 font-extrabold text-white uppercase md:text-md md:justify-evenly md:w-full">
-      <button className="flex-grow uppercase hover:text-lg" onClick={login}>Sign In</button>
+      {!username && <button className="flex-grow uppercase hover:text-lg" onClick={login}>
+        {`Sign In`}
+      </button>}
+      {username && <button className="flex-grow uppercase hover:text-lg" >
+        {username}
+      </button>}
       <button className="flex-grow uppercase hover:text-lg">Documents</button>
       <button className="flex-grow uppercase hover:text-lg">About Us</button>
-      <button className="hidden sm:inline flex-grow uppercase hover:text-lg">Github</button>
+      <button className="hidden sm:inline flex-grow uppercase hover:text-lg">
+        Github
+      </button>
     </div>
   );
 }
