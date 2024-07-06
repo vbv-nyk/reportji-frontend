cd211d58183e5bf4d024be3d35bc408877a1d004
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2024-07-06 16:44:33 +0530

    Responsive landing page

--- app/Components/Landing.tsx
+++ app/Components/Landing.tsx
@@ -7,44 +7,38 @@ import { BACKEND_URL } from "../constants";
 
 export default function Landing() {
   return (
-    <div className="bg-[#01162B] h-screen">
-      <div className="flex items-center justify-around">
-        <Logo2/>
+    <div className="flex flex-col bg-[#01162B] h-screen">
+      <div className="flex h-max items-center justify-around">
+        <Logo2 />
         <Navbar />
       </div>
-      <div className="">
-        <div className="flex flex-col gap-8 justify-center items-center">
-            <img
-              className=""
-              src="images/laptop-report.webp"
-            ></img>
-          <div className="">
-            <div className="text-white font-extrabold text-3xl">Report Making</div>
-            <div className="text-white font-extrabold text-3xl">
-              Made <span className="font-extrabold text-yellow-400 text-3xl">Easier.</span>
+      <div className="md:h-full md:grid md:grid-cols-[1fr_1fr]">
+        <div className="flex flex-col gap-8 justify-center items-center ">
+          <img
+            className="max-h-[400px] lg:hidden"
+            src="images/laptop-report.webp"
+          ></img>
+          <div className="flex flex-col gap-4 ">
+            <div className="flex flex-col">
+              <div className="text-white font-extrabold text-3xl">
+                Report Making
+              </div>
+              <div className="text-white font-extrabold text-3xl">
+                Made{" "}
+                <span className="font-extrabold text-yellow-400 text-3xl">
+                  Easier.
+                </span>
+              </div>
             </div>
+            <Link href={"/choice"}>
+              <ButtonWhite1 />
+            </Link>
           </div>
-          <Link href={"/choice"}>
-            <ButtonWhite1 />
-          </Link>
         </div>
-        <div className="">
-        </div>
-        <div className="hidden">
-          <div className="">
-            <img
-              className=""
-              src="images/guy_left.png"
-            ></img>
-            <img
-              className=""
-              src="images/guy_right.png"
-            ></img>
-            <img
-              className=""
-              src="images/big_guy.jpeg"
-            ></img>
-          </div>
+        <div className="shrink-0 hidden lg:h-full md:grid grid-rows-[25%_25%_25%_25%] grid-cols-3">
+            <img className="row-start-1 row-end-3 col-start-1 col-end-2 z-10 h-full self-center justify-self-center max-h-[150px] aspect-[20/10]" src="images/guy_left.png"></img>
+            <img className="row-start-1 row-end-3 col-start-3 col-end-4 z-10 h-full max-h-[300px] self-center justify-self-start" src="images/guy_right.png"></img>
+            <img className="row-start-1 row-end-5 col-start-1 h-full justify-self-center max-w-3/4 col-end-4 self-center "src="images/big_guy.jpeg"></img>
         </div>
       </div>
     </div>
