173ea4b5a2324212b1e8896bcc8e67838c864920
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2024-07-12 23:01:11 +0530

    Update constants

--- app/constants.tsx
+++ app/constants.tsx
@@ -1,4 +1,4 @@
 export const DEVELOPMENT = false
 export const BACKEND_PORT = `4000`
-export const IP = DEVELOPMENT ?  "new-laptop.tail9e6c6.ts.net": `localhost` 
+export const IP = DEVELOPMENT ?  "reportji.zapto.org": `localhost` 
 export const BACKEND_URL = `http://${IP}:${BACKEND_PORT}`
